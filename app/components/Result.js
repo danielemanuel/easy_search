9d66683c7d028f238df57e0ff8d48fcd6b7ceef3
danielemanuel <daniel.costea@vkernel.ro>
2017-08-18 15:02:17 +0100

    * displayed price as float

--- app/components/Result.js
+++ app/components/Result.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom';
 
 class Result extends Component {
 
+	displayPrice(price) {
+		return (price / 100.00)
+	}
+
 	render() {
 		return( 
 		<div className='in-stock'>
 			<h2><a href='#' >{this.props.product.name}</a></h2>
-				<p>{this.props.product.price}</p>
+				<p>${this.displayPrice(this.props.product.price)}</p>
 				<p>{this.props.product.description}</p>
 		</div>
 
